84bc551f48aa0d2bb3d26fbb487f5f831b1d8b7b
bluerat1976 <osoblivaia76@mail.ru>
2019-04-28 19:21:03 +0300

    fixed read-more-read-less

--- src/js/components/readMoreReadless.js
+++ src/js/components/readMoreReadless.js
@@ -1,19 +1,26 @@
 function toggleContent(evt) {
-  var dots = document.getElementById('dots');
+  var clickEl = evt.target;
 
-  var moreText = document.getElementById('more');
-  var btnText = document.getElementsByClassName('read-more-read-less_cta');
+  if (clickEl.classList.contains('read-more-read-less__cta')) {
+    var currentContainer = clickEl.closest('.read-more-read-less');
 
-  if (dots.style.display === 'none') {
-    dots.style.display = 'inline';
-    btnText.innerHTML = 'Read more';
-    moreText.style.display = 'none';
-  } else {
-    dots.style.display = 'none';
-    btnText.innerHTML = 'Read less';
-    moreText.style.display = 'inline';
+    if (currentContainer) {
+      var dots = currentContainer.getElementsByClassName('read-more-read-less__dots')[0];
+      var moreText = currentContainer.getElementsByClassName('read-more-read-less__more')[0];
+      var buttons = currentContainer.getElementsByClassName('read-more-read-less__cta');
+
+      for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.toggle('hide');
+      }
+
+      dots.classList.toggle('hide');
+      moreText.classList.toggle('hide');
+    }
   }
 }
 
-document.getElementsByClassName('read-more-read-less__container')[0]
-  .addEventListener('click', toggleContent);
+var containers = document.getElementsByClassName('read-more-read-less__container');
+
+if (containers.length) {
+  containers[0].addEventListener('click', toggleContent);
+}
